0d21295dc78dbfcbe5ff94b5c6562dad502807ec
Majo-2306 <maria_castro24@unikino.edu.mx>
2025-02-05 08:03:27 -0700

    Agregar archivos de proyecto.

---
+++ POAgenda/Contacto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POAgenda
+{
+    internal class Contacto
+    {
+        //estos son campos
+        private string _nombre;
+        private string _telefono;
+
+        //estos son permisos para que se puedan ver desde fuera "propiedad"
+        public string Nombre {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+        public string Telefono { 
+            get
+            { 
+                return _telefono;
+            }
+            set
+            {
+                _telefono = value;
+            }
+        }
+        // los campos _nombre y _telefono obtienen la infomacion de los campos y lo que se reciva lo reciben el _nombre y _telefono
+        public Contacto(string nombre, string telefono) 
+        {
+            _nombre = nombre;
+            _telefono = telefono;
+        }
+    }
+}
---
+++ POAgenda/Form1.cs
@@ -0,0 +1,16 @@
+namespace POAgenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Contacto primerContacto = new Contacto("Mariajose", "6221194956");
+            MessageBox.Show($"El telefono de  {primerContacto.Nombre} es : {primerContacto.Telefono}");
+        }
+    }
+}
